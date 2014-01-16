1e1cf5a56dda7deadaf3faf5fe1be0cbd8fa3b40
Rotem Yaari <rotemy@infinidat.com>
2014-01-16 15:07:09 +0200

    Fix packaging manifest

--- munch/__init__.py
+++ munch/__init__.py
@@ -23,7 +23,7 @@
     converted via Munch.to/fromDict().
 """
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ('Munch', 'munchify','unmunchify',)
