0c46109a35eecaa9f76432a7cbbc0d06111d2b9f
Rotem Yaari <rotemy@infinidat.com>
2015-08-25 23:07:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities',
