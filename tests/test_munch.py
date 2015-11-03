2a62f93e0d6cb76d9219a4af96e5421e2ccc5588
Rotem Yaari <rotemy@infinidat.com>
2015-11-03 17:20:08 +0200

    Modernize tests

--- tests/test_munch.py
+++ tests/test_munch.py
@@ -1,19 +1,11 @@
 from munch import Munch
-import unittest
 
+def test_dir():
+    m = Munch(a=1, b=2)
+    assert dir(m) == ['a', 'b']
 
-class MunchTest(unittest.TestCase):
-
-    def test_dir(self):
-        m = Munch(a=1, b=2)
-        self.assertEqual(dir(m), ['a', 'b'])
-
-    def test_repr(self):
-        m = Munch({'a b': 1, 'c': 5})
-        self.assertEqual(dir(m), ['a b', 'c'])
-        self.assertEqual('%r' % m, "Munch({'a b': 1, 'c': 5})")
-        self.assertEqual(m, eval('%r' % m))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_repr():
+    m = Munch({'a b': 1, 'c': 5})
+    assert dir(m) == ['a b', 'c']
+    assert ('%r' % m) == "Munch({'a b': 1, 'c': 5})"
+    assert m == eval(('%r' % m))
