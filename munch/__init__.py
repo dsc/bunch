b9cb29ea6e8b71466eb68c453994a04c51cafc95
Alex Fraser <alex@phatcore.com>
2017-07-06 11:40:47 +1000

    Fix for Python 2.6: str.format must field names

--- munch/__init__.py
+++ munch/__init__.py
@@ -180,7 +180,7 @@ class Munch(dict):
 
             (*) Invertible so long as collection contents are each repr-invertible.
         """
-        return '{}({})'.format(self.__class__.__name__, dict.__repr__(self))
+        return '{0}({1})'.format(self.__class__.__name__, dict.__repr__(self))
 
     def __dir__(self):
         return list(iterkeys(self))
@@ -251,7 +251,7 @@ class DefaultMunch(Munch):
         return type(self).fromDict(self, default=self.__default__)
 
     def __repr__(self):
-        return '{}({!r}, {})'.format(
+        return '{0}({1!r}, {2})'.format(
             type(self).__name__, self.__undefined__, dict.__repr__(self))
 
 
